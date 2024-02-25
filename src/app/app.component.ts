42ec76247e3db6439b7e0d12856e9340474c269c
Okafor David <dokafor@theshippack.com>
2024-02-25 13:19:13 +0100

    feat/mastering pipes in angular js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,66 @@
+import { CurrencyPipe, DatePipe, DecimalPipe, JsonPipe, NgIf, TitleCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ConvertPipe } from './convert.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    TitleCasePipe,
+    DatePipe,
+    CurrencyPipe,
+    DecimalPipe,
+    JsonPipe,
+    ConvertPipe,
+    NgIf
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'pipes';
+
+  name: string = '';
+  date: string = '';
+  amount: number = 0;
+  height: number = 0;
+  miles: number = 0;
+
+  car: {[key:string]: string | number} = {
+    make: "Toyota",
+    model: "Camry",
+    year: 2000
+  }
+
+  onNameChange(event: Event) {
+    const value = (<HTMLInputElement>event.target).value;
+    this.name = value;
+  }
+
+  onDateChange(event: Event) {
+    const value = (<HTMLInputElement>event.target).value;
+    this.date = value;
+  }
+
+  onAmountChange(event: Event) {
+    const value = parseFloat((<HTMLInputElement>event.target).value);
+    if (!isNaN(value)) {
+      this.amount = value;
+    }
+  }
+
+  onHeightChange(event: Event) {
+    const value = parseFloat((<HTMLInputElement>event.target).value);
+    if (!isNaN(value)) {
+      this.height = value;
+    }
+  }
+
+  onMilesChange(event: Event) {
+    const value = parseFloat((<HTMLInputElement>event.target).value);
+    if (!isNaN(value)) {
+      this.miles = value;
+    }
+  }
 }
---
+++ src/app/convert.pipe.ts
@@ -0,0 +1,25 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'convert',
+  standalone: true
+})
+export class ConvertPipe implements PipeTransform {
+
+  transform(value: number, targetUnits: string): number  {
+    if (!value) {
+      return 0;
+    }
+    switch (targetUnits) {
+      case 'km':
+        return value * 1.60934;
+      case 'm':
+        return value * 1.60934 * 1000;
+      case 'cm':
+        return value * 1.60934 * 1000 * 100;
+      default:
+        return value;
+    }
+  }
+
+}
